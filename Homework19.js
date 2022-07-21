14d60dc422c3a2fd39bd803f52580b87120de119
KnarikPahlevanyan <knarikpahlevanyan@gmail.com>
2022-07-21 14:02:44 +0400

    Login Page

---
+++ Homework19.js
@@ -0,0 +1,16 @@
+
+
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const ptext = document.querySelector("#error");
+const submit=document.querySelector("#submit");
+
+
+ function login(){
+     if(username === admin && password === adminnn){
+            console.log("You Are Welcome")
+     }else{
+            console.log("ERROR");
+        }
+        window.location = "homework19.html"
+    };
